f86073d03e67939b4461dccbe40a0b0ade421b74
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-08-03 00:23:03 +0200

    Provides minified and non-minified versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 gulp.task('compress', function() {
   return gulp.src('src/*.js')
     .pipe(uglify())
+    .pipe(rename({
+     extname: '.min.js'
+    }))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['compress']);
+gulp.task('copy', function() {
+  return gulp.src('src/*.js')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['copy', 'compress']);
